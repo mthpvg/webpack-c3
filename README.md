765408cc4744e55c21af0f164b06f6ba18a906d2
Mathieu Pavageau <mth.pvg@gmail.com>
2018-01-28 09:52:38 +0100

    chore: first commit

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # webpack-c3
+
+## Preview
+![Screenshot](./resources/screenshot.png)
+
+## Quick start
+```bash
+npm install
+# development
+npm start
+# or "production"
+npm run build
+```
+
+## Based on
+http://c3js.org/gettingstarted.html
---
+++ app/index.js
@@ -0,0 +1,15 @@
+require('./index.css')
+
+import * as c3 from "c3";
+// import { selectAll } from 'd3-selection'
+
+
+var chart = c3.generate({
+    bindto: '#chart',
+    data: {
+      columns: [
+        ['data1', 30, 200, 100, 400, 150, 250],
+        ['data2', 50, 20, 10, 40, 15, 25]
+      ]
+    }
+});
